d06a37bf0bd0fe0c2883a2e51ac375728ff8dc42
Abiyoso Wibowo <abiyosowibowo@gmail.com>
2026-03-02 09:26:41 +0700

    Initial commit

---
+++ Praktikum_1/Mobil.java
@@ -0,0 +1,31 @@
+package Semester_2.Praktikum_1;
+
+public class Mobil {
+    private String noPlat;
+    private String warna;
+    private String manufaktur;
+    private int kecepatan;
+
+    public void setNoPlat(String s) {
+        noPlat = s;
+    }
+
+    public void setWarna(String s) {
+        warna = s;
+    }
+
+    public void setManufaktur(String s) {
+        manufaktur = s;
+    }
+
+    public void setKecepatan(int i) {
+        kecepatan = i;
+    }
+
+    public void displayMessage () {
+        System.out.println("Mobil anda adalah bermerek "+manufaktur);
+        System.out.println("Mempunyai nomor plat "+noPlat);
+        System.out.println("serta memiliki warna "+warna);
+        System.out.println("dan mampu menempuh kecepatan "+kecepatan);
+    }
+}
