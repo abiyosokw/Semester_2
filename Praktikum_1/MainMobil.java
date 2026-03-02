53b70a3296cacf3de45c1c207de26c6b04cebcdd
Abiyoso Wibowo <abiyosowibowo@gmail.com>
2026-03-02 09:52:38 +0700

    Praktikum 1 (Soal 3)
    Rubah kode pada mainMobil diatas menjadi proses meminta masukan dari user dan buat
    menjadi interaktif!

--- Praktikum_1/MainMobil.java
+++ Praktikum_1/MainMobil.java
@@ -1,7 +1,11 @@
 package Semester_2.Praktikum_1;
+import java.util.Scanner;
 
 public class MainMobil { 
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        /* 
         //instan objek bernama m1
         Mobil m1 = new Mobil();
         m1.setKecepatan(50);
@@ -10,6 +14,7 @@ public class MainMobil {
         m1.setWarna("Merah");
         m1.displayMessage();
         System.out.println("================");
+
         //instan objek baru bernama m2
         Mobil m2 = new Mobil();
         m2.setKecepatan(100);
@@ -18,10 +23,30 @@ public class MainMobil {
         m2.setWarna("Pink");
         m2.displayMessage();
         System.out.println("================");
+
         //merubah warna dari objek m1
 System.out.println("mobil pada objek m1 di rubah menjadi warna hijau");
         m1.setWarna("Hijau");
         //menampilkan hasil perubahan
         m1.displayMessage();
+        */
+
+        Mobil m3 = new Mobil();
+        System.out.println("Masukkan kecepatan mobil :");
+        int kecepatan = sc.nextInt();
+        sc.nextLine();
+        m3.setKecepatan(kecepatan);
+        System.out.println("Masukkan manufaktur mobil :");
+        String manufaktur = sc.nextLine();
+        m3.setManufaktur(manufaktur);
+        System.out.println("Masukkan nomor Plat mobil :");
+        String noPlat = sc.nextLine();
+        m3.setNoPlat(noPlat);
+        System.out.println("Masukkan warna mobil :");
+        String warna = sc.nextLine();
+        m3.setWarna(warna);
+        m3.displayMessage();
+
+        sc.close();
     }
 }
