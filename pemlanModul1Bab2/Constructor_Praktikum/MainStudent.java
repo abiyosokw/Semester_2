fbe2f816e616767199c4fc88d356bc368518fc83
Abiyoso Wibowo <abiyosowibowo@gmail.com>
2026-03-11 14:10:46 +0700

    Pratikum 2 (Soal 4 Constructor)
    Bagaimana cara memasukkan jumlah siswa sesuai dengan keinginan user? Tuliskan kodenya dengan inputan user yang interaktif! (key : menggunakan array)

--- pemlanModul1Bab2/Constructor_Praktikum/MainStudent.java
+++ pemlanModul1Bab2/Constructor_Praktikum/MainStudent.java
@@ -1,36 +1,36 @@
 package Semester_2.pemlanModul1Bab2.Constructor_Praktikum;
+import java.util.Scanner;
 
 public class MainStudent {
         public static void main(String[] args) {
-        Student anna = new Student();
-        anna.setName("Anna");
-        anna.setAddress("Malang");
-        anna.setAge(20);
-        anna.setMath(100);
-        anna.setScience(89);
-        anna.setEnglish(80);
-        anna.displayMessage();
-        
-        // //siswa dengan nama anna dirubah informasi alamat dan umurnya melalui constructor
-        // System.out.println("===================");
-        // anna = new Student(100, 91, 89);
-        // anna.setName("Anna");
-        // anna.setAddress("Surabaya");
-        // anna.setAge(21);
-        // anna.displayMessage();  
-        
-         //menggunakan constructor lain
-        System.out.println("===================");
-        Student chris = new Student("Chris", "Kediri", 21);
-        chris.setMath(50);
-        chris.setScience(60);
-        chris.setEnglish(50);
-        chris.displayMessage();
+        Scanner sc = new Scanner(System.in);
 
-        // //siswa denagan nama chris dirubah informasi alamat dan umurnya melalui method
-        // System.out.println("===================");
-        // chris.setAddress("Surabaya");
-        // chris.setAge(22);
-        // chris.displayMessage();
+        System.out.print("Masukkan jumlah siswa yang ingin diinput: ");
+        int jumlahSiswa = sc.nextInt();
+        sc.nextLine();
+        Student[] daftarSiswa = new Student[jumlahSiswa];
+
+        for (int i = 0; i < jumlahSiswa; i++) {
+            System.out.println("Masukkan data ke-"+(i+1)+":");
+            System.out.print("Nama: "); String nama = sc.nextLine();
+            System.out.print("Alamat: "); String alamat = sc.nextLine();
+            System.out.print("Umur: "); int umur = sc.nextInt();
+            System.out.print("Nilai Matematika: "); int math = sc.nextInt();
+            System.out.print("Nilai Bahasa Inggris: "); int english = sc.nextInt();
+            System.out.print("Nilai IPA: "); int science = sc.nextInt();
+            sc.nextLine();
+
+            daftarSiswa[i] = new Student(nama, alamat, umur);
+            daftarSiswa[i].setMath(math);
+            daftarSiswa[i].setEnglish(english);
+            daftarSiswa[i].setScience(science);
+        }
+
+        System.out.println("\nData Siswa:");
+        for (int i = 0; i < daftarSiswa.length; i++) {
+            daftarSiswa[i].displayMessage();
+            System.out.println();
+        }
+        sc.close();
     }
 }
