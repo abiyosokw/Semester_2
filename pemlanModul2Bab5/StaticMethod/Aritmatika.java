90400c07f9ee22cf0ad4068890281cb2807ccb8c
Abiyoso Wibowo <abiyosowibowo@gmail.com>
2026-03-24 15:48:12 +0700

    Pratikum 5 (Soal 5 Static Method)
    Jika pada tubuh method hitungPerkalian ditambahkan syntax hitungPenjumlahan(a,b) apa yang terjadi? Jelaskan?

--- pemlanModul2Bab5/StaticMethod/Aritmatika.java
+++ pemlanModul2Bab5/StaticMethod/Aritmatika.java
@@ -4,12 +4,14 @@ public class Aritmatika {
     public void hitungPenjumlahan(int a,int b){
         int nilai = a+b;
         System.out.println("nilai penjumlahan adalah: "+nilai);
-        hitungPerkalian(a, b);
     }
     
     public static void hitungPerkalian(int a, int b){
         int nilai = a*b;
         System.out.println("nilai perkalian adalah: "+nilai);
+
+        Aritmatika obj = new Aritmatika();
+        obj.hitungPenjumlahan(a, b); 
     }
 
     public static void hitungPengurangan(int a, int b){
