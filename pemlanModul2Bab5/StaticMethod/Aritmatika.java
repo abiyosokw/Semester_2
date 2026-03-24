f3ff5c1dda79182355cd48553bda209932c05dee
Abiyoso Wibowo <abiyosowibowo@gmail.com>
2026-03-24 15:34:57 +0700

    Commit Static Method

---
+++ pemlanModul2Bab5/StaticMethod/Aritmatika.java
@@ -0,0 +1,18 @@
+package Semester_2.pemlanModul2Bab5.StaticMethod;
+
+public class Aritmatika {
+    public void hitungPenjumlahan(int a,int b){
+        int nilai = a+b;
+        System.out.println("nilai penjumlahan adalah: "+nilai);
+    }
+    
+    public static void hitungPerkalian(int a, int b){
+        int nilai = a*b;
+        System.out.println("nilai perkalian adalah: "+nilai);
+    }
+
+    public static void hitungPengurangan(int a, int b){
+        int nilai = a-b;
+        System.out.println("nilai pengurangan adalah: "+nilai);
+    }
+}
